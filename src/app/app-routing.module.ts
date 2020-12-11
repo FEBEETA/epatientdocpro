38cda6038748e299bc28230f673c1f6a806730f3
Justin Xavier <justin.deimos@gmail.com>
2020-12-11 23:14:02 +0530

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PatientDetailsComponent } from './patient-details/patient-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PatientComponent } from './patient/patient.component';
 
+const routes: Routes = [
+{ path: '', redirectTo: '', pathMatch: 'full'},
+{ path: 'patient', component: PatientComponent},
+{ path: 'patientdetails', component: PatientDetailsComponent},
+{ path: '**', component: PageNotFoundComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './intro/intro.component';
+import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { AdminComponent } from './admin/admin.component';
+import { PatientDetailsComponent } from './patient-details/patient-details.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    PatientComponent,
+    DoctorComponent,
+    AdminComponent,
+    PatientDetailsComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
