e252e0f7fe8270c50e1ec5e7f210612d5ad069e3
Justin Xavier <justin.deimos@gmail.com>
2020-12-12 09:10:10 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { PatientDetailsComponent } from './patient-details/patient-details.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { PatientComponent } from './patient/patient.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { AdminComponent } from './admin/admin.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
 { path: '', redirectTo: '', pathMatch: 'full'},
+{ path: 'intro', component: IntroComponent},
 { path: 'patient', component: PatientComponent},
 { path: 'patientdetails', component: PatientDetailsComponent},
+{ path: 'doctor', component: DoctorComponent},
+{ path: 'admin', component: AdminComponent},
 { path: '**', component: PageNotFoundComponent}
 
 ];
@@ -16,4 +22,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
